051c884bdcfc8072976c5986376a67f85dbd5ecb
tgasla <theodoros.aslanidis@ucdconnect.ie>
2025-05-09 13:52:21 +0100

    Delete deprecated directory 'deprecated-gvirtus-role-detector'. Rename pre-release version labels in all components to 0.1.0-alpha

--- deprecated-gvirtus-role-detector/docker_build_push.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker buildx build --platform linux/arm64,linux/amd64 --push -t taslanidis/gvirtus-node-setup --build-arg USER=$(id -un) --build-arg UID=$(id -u) --build-arg GID=$(id -g) .
